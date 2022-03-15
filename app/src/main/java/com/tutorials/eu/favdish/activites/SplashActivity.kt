f39430f5bebfd235d0696474e7948c4d8f5a5fdf
liu.yunxi <leoyunxi@gmail.com>
2022-03-15 15:51:36 +0900

    replace the app default icon

--- app/src/main/java/com/tutorials/eu/favdish/activites/SplashActivity.kt
+++ app/src/main/java/com/tutorials/eu/favdish/activites/SplashActivity.kt
@@ -25,8 +25,6 @@ class SplashActivity : AppCompatActivity() {
         val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(splashBinding.root)
 
-        // TODO Step 1: Make the Splash Activity as a full screen view that means hide the Status Bar.
-        // START
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
         } else {
@@ -36,16 +34,10 @@ class SplashActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
-        // END
 
-        // TODO Step 5: Create a access variable for Animation as below. Uncomment the TextView binding code and apply the animation to it.
-        // START
         val splashAnimation = AnimationUtils.loadAnimation(this@SplashActivity, R.anim.anim_splash)
         splashBinding.tvAppName.animation = splashAnimation
-        // END
 
-        // TODO Step 7: We will see if you want to perform any action after animation completion with the callbacks is as below.
-        // START
         splashAnimation.setAnimationListener(object : Animation.AnimationListener {
             override fun onAnimationStart(animation: Animation?) {
                 // "Add the code that you want to execute when animation starts")
@@ -54,20 +46,15 @@ class SplashActivity : AppCompatActivity() {
             override fun onAnimationEnd(animation: Animation?) {
                 // "Add the code that you want to execute when animation ends")
 
-                // TODO Step 8: Once the animation completes we will navigate it to the Main Activity with delay 1 second using Handler.
-                // START
                 Handler(Looper.getMainLooper()).postDelayed({
                     startActivity(Intent(this@SplashActivity, MainActivity::class.java))
                     finish()
                 }, 1000)
-                // END
             }
 
             override fun onAnimationRepeat(animation: Animation?) {
                 // "Add the code that you want to execute when animation repeats")
             }
         })
-        // END
-
     }
 }
